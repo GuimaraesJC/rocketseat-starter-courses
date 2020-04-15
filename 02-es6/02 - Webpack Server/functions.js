00db8a4604d787b0fd34a1a86b1b02a4cd482d2d
Jean Carlos Costa <jean.costa@sankhya.com.br>
2020-04-14 22:14:06 -0300

    Soluções para o exercício do 2º módulo do curso de JavaScript ES6.

---
+++ 02-es6/02 - Webpack Server/functions.js
@@ -0,0 +1,11 @@
+/**
+ * Arquivo criado como parte da resolução do exercício 01 do módulo 02 (Webpack Server)
+ */
+
+export const idade = 23;
+
+export default class Usuario {
+    static info() {
+        console.log('Apenas teste');
+    }
+}
